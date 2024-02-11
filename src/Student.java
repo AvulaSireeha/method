c32336b4a94a3e4c8e6a7be392d53dcd9025a9a7
dell <avulasireesha0628@gmail.com>
2024-02-11 11:13:18 +0530

    sanath

---
+++ src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    public String name;
+    public string fatherName;
+
+
+    public Student(String name, string fatherName) {
+        this.name = name;
+        this.fatherName = fatherName;
+    }
+}
